746e4027981c969b4932c918fe35dfaeb16fb804
Ogem4tech <ogem4tech@gmail.com>
2023-04-21 14:58:30 -0700

    positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,13 +15,13 @@ int main(void)
 	{
 		printf("%d is positive\n", n);
 	}
-	else
-	{
-		printf("%d is negative\n", n);
-	}
 	if (n == 0)
 	{
 		printf("%d is zero\n", n);
+	}
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
