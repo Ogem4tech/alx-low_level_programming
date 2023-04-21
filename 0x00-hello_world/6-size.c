4a760ae3cfcfcfa291a4b3f819ab515a56e377ec
Ogem4tech <ogem4tech@gmail.com>
2023-04-20 18:45:03 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("Size of a char: %i byte(s)\n", sizeofchar(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
+	return (0);
+}
