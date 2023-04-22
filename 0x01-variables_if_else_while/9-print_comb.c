93a4c0005dc7858cab47baa229cfc642c6c77d92
Ogem4tech <ogem4tech@gmail.com>
2023-04-22 08:08:31 -0700

    print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,11 +7,11 @@ int main(void)
 {
 	int num;
 
-	for (num = 27; num < 37; num++)
+	for (num = 48; num <= 58; num++)
 	{
 	putchar(num);
 	}
-	if (num != 36)
+	if (num != 57)
 	{
 	putchar(',');
 	putchar(' ');
