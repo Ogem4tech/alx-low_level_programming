5484e51ba5665da79ff19a9b00592c496b3e9c0c
Ogem4tech <ogem4tech@gmail.com>
2023-04-25 12:19:06 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - prints all alphabet in lowercase
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	_putchar(letter);
+	}
+	_putchar('\n');
+	
+}
+
