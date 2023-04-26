0369c65edc28b8184442c6d45dc55b4a3718410e
Ogem4tech <ogem4tech@gmail.com>
2023-04-25 17:31:26 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - Print the sign of a number
+ * @n: The number to be checked
+ * Return: 1 for the positive num, -1 for negative num or 0  for any other num.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar(43);
+	return (1);
+	}
+	else if (n < 0)
+	{
+	_putchar(45);
+	return (-1);
+	}
+	else
+	{
+	_putchar(48);
+	return (0);
+	}
+}
