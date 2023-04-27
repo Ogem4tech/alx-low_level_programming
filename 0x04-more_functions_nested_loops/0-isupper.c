33c63f35a511f8ed837352a9ed2188708d3d0f2b
Ogem4tech <ogem4tech@gmail.com>
2023-04-27 13:38:43 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * _isupper - Check if letter is uppercase
+  * @c: The number to be checked
+  * Return: 1 for uppercase or 0 for otherwise
+  */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
