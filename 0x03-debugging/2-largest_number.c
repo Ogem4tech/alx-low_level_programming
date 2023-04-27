b9f68126a544c5903d8e473a3fed9fdc3e6951f8
Ogem4tech <ogem4tech@gmail.com>
2023-04-26 23:10:48 -0700

    push task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 * largest_number - returns the largest of 3 numbers
 * @a: first integer
@@ -12,17 +13,21 @@ int largest_number(int a, int b, int c)
 	int largest;
 
 	if (a > b && a > c)
-	{
+{
 	largest = a;
-	}
-	else if (b > a)
-	{
-	largest = b;
-	}
-	else
-	{
+}
+	else if (a > b && c > a)
+{
 	largest = c;
-	}
+}
+	else if (b > c)
+{
+	largest = b;
+}
+	else
+{
+	largest = c;
+}
 
 	return (largest);
 }
