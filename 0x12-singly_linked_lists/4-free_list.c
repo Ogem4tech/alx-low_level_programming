3f4f75362630ee8343daa0fe4ba267ce4c1cfce5
Ogem4tech <ogem4tech@gmail.com>
2023-06-01 17:49:13 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - Release the memory allocated for a linked list
+ *
+ * @head: A pointer to the first node of the list to be freed
+ */
+void free_list(list_t *head)
+{
+	if (head)
+	{
+		free_list(head->next);
+		if (head->str)
+			free(head->str);
+		free(head);
+	}
+}
